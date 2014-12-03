49ff1e9812aaf0f0cac952b77154437d7c417cca
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-03 20:23:43 +0000

    trying out roles via hiera

---
+++ site/profile/manifests/mcollective.pp
@@ -0,0 +1,4 @@
+    class { '::mcollective':
+        middleware_hosts => [ 'dxstg-ops-puppet01.devaws.dataxu.net' ],
+    }
+
---
+++ site/profile/manifests/ntp_server.pp
@@ -0,0 +1,4 @@
+    class { '::ntp':
+        restrict => ['172.17.68.0 mask 255.255.252.0 nomodify notrap','10.0.0.0 mask 255.0.0.0 nomodify notrap'],
+        udlc     => true,
+    }
