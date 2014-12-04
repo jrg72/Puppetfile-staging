e9f0d85ee855ccc59825bd9861c5705fc3a908b6
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-04 21:35:36 +0000

    add packages for mco clients

--- site/profile/manifests/mcollective.pp
+++ site/profile/manifests/mcollective.pp
@@ -1,8 +1,19 @@
 class profile::mcollective {
+    $packages = [ 'mcollective',
+                'mcollective-service-agent',
+                'mcollective-package-agent',
+                'mcollective-sysctl-data',
+                'mcollective-nettest-agent',
+                'mcollective-puppet-agent',
+                'mcollective-filemgr-agent',
+                'mcollective-iptables-agent',
+                ]
+
     class { '::mcollective':
         connector        => rabbitmq,
         middleware_hosts => [ 'dxstg-ops-puppet01.devaws.dataxu.net' ],
-    }
+    } ->
+    package { $packages : ensure => latest }
 }
 
 class profile::mcollective::client {
