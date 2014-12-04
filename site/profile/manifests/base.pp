54f196bc4dddfef4daa545f8bd3e8d438adf4dcd
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-04 16:51:33 +0000

    setting up bare metal AWS routes

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -21,5 +21,21 @@ class profile::base {
     mode   => '0644',
   }
 
+  if str2bool("$is_virtual") {
+    notice("Virtual Machine, not setting route to AWS.")
+  }
+
+  else {
+    notice("Bare Metal Box, setting route to AWS."),
+    network_route { '172.17.64.0/19':
+      ensure    => 'present',
+      gateway   => "${::network_internal_gw}
+      interface => 'bond0',
+      netmask   => '19',
+      network   => '172.17.64.0',
+    }
+  }
+
+
 }
 
