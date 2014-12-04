a4d50227b1ca9031b47b8caf7f743f708e94f0d1
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-04 17:08:20 +0000

    try to purge route

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -27,6 +27,16 @@ class profile::base {
 
   else {
     notify {"Bare Metal Box, setting route to AWS.":}
+    network_route { '10.0.0.0/8':
+      ensure    => 'purged'
+    } ->
+    network_route { '10.0.0.0/8':
+      ensure    => 'present',
+      gateway   => "${::network_internal_gw}",
+      interface => 'bond0',
+      netmask   => '8',
+      network   => '10.0.0.0',
+    } ->
     network_route { '172.17.64.0/19':
       ensure    => 'present',
       gateway   => "${::network_internal_gw}",
