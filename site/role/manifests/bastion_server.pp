65cde9e7bf14c65c07b0aaab0b89dfaa5c4b8e06
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-05 17:18:40 +0000

    add mco clients to bastion

--- site/role/manifests/bastion_server.pp
+++ site/role/manifests/bastion_server.pp
@@ -1,3 +1,13 @@
 class role::bastion_server {
+    $packages = [ 'mcollective-service-client',
+                'mcollective-package-client',
+                'mcollective-nettest-client',
+                'mcollective-puppet-client',
+                'mcollective-filemgr-client',
+                'mcollective-iptables-client',
+                ]
+
+    package { $packages : ensure => latest }
+
 }
 
