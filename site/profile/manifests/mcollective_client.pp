f4fd51d475c3b435ef5ca3ed10a92f48f5356f23
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-05 16:55:40 +0000

    removing unneeded params .pps, we do this in hiera now

--- site/profile/manifests/mcollective_client.pp
+++
@@ -1,7 +0,0 @@
-class profile::mcollective_client {
-    class { '::mcollective':
-        connector        => rabbitmq,
-        middleware_hosts => [ 'dxstg-ops-puppet01.devaws.dataxu.net' ],
-        client           => true,
-    }
-}
--- site/profile/manifests/ntp_server.pp
+++
@@ -1,6 +0,0 @@
-class profile::ntp_server {
-    class { '::ntp':
-        restrict => ['172.17.68.0 mask 255.255.252.0 nomodify notrap','10.0.0.0 mask 255.0.0.0 nomodify notrap'],
-        udlc     => true,
-    }
-}
