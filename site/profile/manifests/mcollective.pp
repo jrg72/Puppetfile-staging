8bc04b34ddac7cbbb1d892e869bd279a4f38a17e
root <root@dxstg-ops-puppet01.devaws.dataxu.net>
2014-12-05 17:47:49 +0000

    set up plugins

--- site/profile/manifests/mcollective.pp
+++ site/profile/manifests/mcollective.pp
@@ -1,17 +1,7 @@
 class profile::mcollective {
-    $packages = [ 'mcollective-service-agent',
-                'mcollective-package-agent',
-                'mcollective-sysctl-data',
-                'mcollective-nettest-agent',
-                'mcollective-puppet-agent',
-                'mcollective-filemgr-agent',
-                'mcollective-iptables-agent',
-                ]
-
     class { '::mcollective':
         connector        => rabbitmq,
         middleware_hosts => [ 'dxstg-ops-puppet01.devaws.dataxu.net' ],
     } ->
-    package { $packages : ensure => latest }
 }
 
